ca6fc24d9c085311c1171701658ec3bf915d7b2f
victordruc <70332522+victordruc@users.noreply.github.com>
2020-12-09 22:35:48 +0200

    Add files via upload

---
+++ development/appico-screenshots/screenshots.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.g-carousel').slick({
+        slidesToShow: 5,
+        infinite: true,
+        arrows: true,
+        nextArrow: '<i class="fa fa-angle-right" aria-hidden="true"></i>',
+        prevArrow: '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        // dots: true,
+    });
+  });
